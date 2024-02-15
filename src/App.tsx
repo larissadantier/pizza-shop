eeba22a3493e52fc93d859973e41f876fb5b2fdd
larissadantier <larissa_dantier@hotmail.com>
2024-02-14 23:31:23 -0300

    feat: create initial page SignIn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './global.css'
 
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 import { router } from '@/routes/index.tsx'
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+      <Toaster position="top-right" richColors />
+    </HelmetProvider>
+  )
 }
 
 export default App
