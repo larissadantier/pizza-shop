335aab7578c54920e98dfaaaed8b2d970f801389
Larissa Dantier <larissa_dantier@hotmail.com>
2024-02-27 22:54:39 -0300

    chore: configure query and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
@@ -7,15 +8,18 @@ import { Toaster } from 'sonner'
 import { router } from '@/routes/index.tsx'
 
 import { ThemeProvider } from './contexts/themeProvider'
+import { queryClient } from './lib/react-query'
 
 function App() {
   return (
     <HelmetProvider>
-      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
-        <Helmet titleTemplate="%s | pizza.shop" />
-        <RouterProvider router={router} />
-        <Toaster position="top-right" richColors />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+          <Helmet titleTemplate="%s | pizza.shop" />
+          <RouterProvider router={router} />
+          <Toaster position="top-right" richColors />
+        </ThemeProvider>
+      </QueryClientProvider>
     </HelmetProvider>
   )
 }
